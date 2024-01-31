4aa848681979c28ac73e09880e8be94499a64ffa
pabcrudel <pabcrudel@gmail.com>
2024-01-31 11:20:25 +0100

    feature: Create "Hello, World!" with Next.js

---
+++ src/app/layout.jsx
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Hello, World!</title>
+      </head>
+
+      <body>{ children }</body>
+    </html>
+  );
+}
