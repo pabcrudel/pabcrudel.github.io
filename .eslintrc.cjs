deb2f969cc627b611c46fc8094df5e70ce29f17c
pabcrudel <pabcrudel@gmail.com>
2024-03-19 21:12:31 +0100

    chore: Installed and configured Eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/** @type {import("eslint").Linter.Config} */
+
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+    "semistandard"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaVersion: "latest"
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"]
+      }
+    }
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    quotes: ["error", "double"]
+  }
+};
