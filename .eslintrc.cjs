42853a19c4d181bd2f0cabe60b8c5748871b99ec
pabcrudel <pabcrudel@gmail.com>
2024-03-20 21:06:38 +0100

    style: Require trailing commas in multiline elements

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
 /** @type {import("eslint").Linter.Config} */
-
 module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:astro/recommended",
     "plugin:astro/jsx-a11y-recommended",
-    "semistandard"
+    "semistandard",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
     sourceType: "module",
-    ecmaVersion: "latest"
+    ecmaVersion: "latest",
   },
   overrides: [
     {
@@ -20,15 +19,14 @@ module.exports = {
       parser: "astro-eslint-parser",
       parserOptions: {
         parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"]
-      }
-    }
+        extraFileExtensions: [".astro"],
+      },
+    },
   ],
   plugins: ["@typescript-eslint"],
   rules: {
-    quotes: ["error", "double"]
+    quotes: ["error", "double"],
+    "comma-dangle": ["error", "always-multiline"],
   },
-  ignorePatterns: [
-    "env.d.ts"
-  ]
+  ignorePatterns: ["env.d.ts"],
 };
